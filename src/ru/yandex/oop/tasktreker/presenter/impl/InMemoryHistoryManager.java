5005967f2fed5eb493ed9ec26ec523e81fd10f31
Chicherin Egor <chicherinegor@gmail.com>
2023-01-19 12:36:42 +0300

    e com

--- src/ru/yandex/oop/tasktreker/presenter/impl/InMemoryHistoryManager.java
+++ src/ru/yandex/oop/tasktreker/presenter/impl/InMemoryHistoryManager.java
@@ -13,7 +13,7 @@ public class InMemoryHistoryManager implements HistoryManager {
     private List<Task> historyTaskList;
 
     public InMemoryHistoryManager() {
-        this.historyTaskList = new CustomLinkedList();
+        this.historyTaskList = new LinkedList<>();
     }
     public void remove(int id){ // это к 5-му тз
 
@@ -33,28 +33,6 @@ public class InMemoryHistoryManager implements HistoryManager {
     public List<Task> getHistory() {
 
         return new ArrayList<>(historyTaskList);
-    }
-
-    public class CustomLinkedList  {
-        // айди, индекс в списке
-        private Map<Integer, Node> idIndexMap;
-        private List<Node> taskList;
-
-        public CustomLinkedList() {
-            this.idIndexMap = new HashMap<>();
-            this.taskList = new LinkedList<>();
-        }
-
-        public void linkLast(Task task) {
-            taskList.add(new Node());
-            idIndexMap.put(task.getId(), );
-        }
-
-        public List<Task> gatTasks() {
-            return new ArrayList<>(taskList);
-        }
-
-
     }
 
 }
