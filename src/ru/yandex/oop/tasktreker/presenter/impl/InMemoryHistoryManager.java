2a7e2b59328d76d84b9ca6ba7a85296053c29a11
Chicherin Egor <chicherinegor@gmail.com>
2023-01-20 12:54:58 +0300

    e com2

--- src/ru/yandex/oop/tasktreker/presenter/impl/InMemoryHistoryManager.java
+++ src/ru/yandex/oop/tasktreker/presenter/impl/InMemoryHistoryManager.java
@@ -10,29 +10,63 @@ import java.util.*;
 
 public class InMemoryHistoryManager implements HistoryManager {
 
-    private List<Task> historyTaskList;
+    Map<Integer, Node> historyTaskMap = new LinkedHashMap<>();
 
-    public InMemoryHistoryManager() {
-        this.historyTaskList = new LinkedList<>();
-    }
+    private Node<Task> first;
+    private Node<Task> last;
+
+
     public void remove(int id){ // это к 5-му тз
-
+        historyTaskMap.remove(id);
     }
 
+    public void removeNode(Node node) {
+        Task task = (Task) node.getTask();
+        historyTaskMap.remove(task.getId());
+    }
 
+    public void linkLast(Task task) {
+        Node node = new Node(task);
+        if (historyTaskMap.isEmpty()) {
+            first = node;
+        }
+        historyTaskMap.put(task.getId(), node);
+        last = node;
+    }
 
     @Override
     public void add(Task task) {
-        if (historyTaskList.size() == 10) {
-            historyTaskList.remove(0);
-        }
-        historyTaskList.add(task);
+        remove(task.getId());
+        linkLast(task);
     }
 
     @Override
     public List<Task> getHistory() {
+        List<Task> list = new ArrayList<>();
+        for (Map.Entry<Integer, Node> pair : historyTaskMap.entrySet()) {
+            list.add((Task) pair.getValue().getTask());
+        }
+        return list;
+    }
 
-        return new ArrayList<>(historyTaskList);
+    public List<Task> getTasks() {
+        List<Task> list = new ArrayList<>();
+        for (Map.Entry<Integer, Node> pair : historyTaskMap.entrySet()) {
+            list.add((Task) pair.getValue().getTask());
+        }
+        return list;
+    }
+
+    public class Node<Task> {
+        private final Task task;
+
+        public Node(Task task) {
+            this.task = task;
+        }
+
+        public Task getTask() {
+            return task;
+        }
     }
 
 }
