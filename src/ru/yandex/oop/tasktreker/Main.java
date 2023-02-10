53a0ef66a83c31c4cbe3c297cbc6995d251fd341
TomilovES <blat13@gmail.com>
2023-02-10 20:46:07 +0300

    тз_6 проблемы история пишется с печатью

--- src/ru/yandex/oop/tasktreker/Main.java
+++ src/ru/yandex/oop/tasktreker/Main.java
@@ -13,6 +13,7 @@ public class Main {
 
 
     public static void main(String[] args) {
+
         TaskManager manager = new InMemoryTaskManager();
         HistoryManager historyManager = manager.getHistoryManager();
 //        System.out.println("**********************************TASK**********************************");
--- src/ru/yandex/oop/tasktreker/presenter/impl/FileBackedTasksManager.java
+++ src/ru/yandex/oop/tasktreker/presenter/impl/FileBackedTasksManager.java
@@ -9,7 +9,6 @@ import ru.yandex.oop.tasktreker.model.enums.TaskStatus;
 import ru.yandex.oop.tasktreker.model.enums.TaskType;
 import ru.yandex.oop.tasktreker.presenter.HistoryManager;
 import ru.yandex.oop.tasktreker.presenter.TaskManager;
-import ru.yandex.oop.tasktreker.presenter.util.Managers;
 
 import java.io.BufferedWriter;
 import java.io.File;
@@ -27,8 +26,6 @@ public class FileBackedTasksManager extends InMemoryTaskManager {
     private static String file = "src/ru/yandex/oop/tasktreker/resource/file.csv";
 
 
-
-
     public FileBackedTasksManager() {
         super();
     }
@@ -37,11 +34,13 @@ public class FileBackedTasksManager extends InMemoryTaskManager {
 
     }// как сделать конструктор для loadFromFile
 
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         TaskManager manager = new FileBackedTasksManager();
+        HistoryManager historyManager = manager.getHistoryManager();
 
 
-       //**********************************TASK*************************************;
+        //**********************************TASK*************************************;
         Task task1 = new Task("Тренировка", "день грудь-плечи", TaskType.TASK);
         Task task2 = new Task("Тренировка", "день ноги", TaskType.TASK);
         manager.createTaskAndReturnId(task1);
@@ -56,8 +55,8 @@ public class FileBackedTasksManager extends InMemoryTaskManager {
         epicTask2.setId(manager.createTaskAndReturnId(epicTask2));
 
 //       **********************************SUBTASK**********************************
-        SubTask subTask1 = new SubTask("Деньги", "Накопить бабло",  TaskType.SUBTASK, epicTask1.getId());
-        SubTask subTask2 = new SubTask("Квартира", "Найти хату",  TaskType.SUBTASK, epicTask1.getId());
+        SubTask subTask1 = new SubTask("Деньги", "Накопить бабло", TaskType.SUBTASK, epicTask1.getId());
+        SubTask subTask2 = new SubTask("Квартира", "Найти хату", TaskType.SUBTASK, epicTask1.getId());
         SubTask subTask3 = new SubTask("Банк", "Найти банк с наименьшим %", TaskType.SUBTASK, epicTask1.getId());
         subTask1.setStatus(TaskStatus.IN_PROGRESS);
         subTask2.setStatus(TaskStatus.DONE);
@@ -69,15 +68,16 @@ public class FileBackedTasksManager extends InMemoryTaskManager {
         Task t1 = manager.getAnyTask(4);
         Task t2 = manager.getAnyTask(2);
         Task t3 = manager.getAnyTask(1);
-        Task t4 = manager.getAnyTask(3);
-        Task t5 = manager.getAnyTask(5);
+
 
         t1 = manager.getAnyTask(5);
         t1 = manager.getAnyTask(5);
         t1 = manager.getAnyTask(5);
-        t1 = manager.getAnyTask(5);
-        t1 = manager.getAnyTask(5);
 
+
+        List<Task> history = historyManager.getHistory();
+
+        history.forEach(System.out::println);
 
     }
 
@@ -91,16 +91,16 @@ public class FileBackedTasksManager extends InMemoryTaskManager {
                 if (!lines[i].isBlank() && i != lines.length - 1) {
                     fromFile.createTaskAndReturnId(fromFile.fromString(lines[i]));
                 }
-                if (i == lines.length -1) {
-                    List<Integer> historyList = historyFromString(lines[lines.length -1]);
+                if (i == lines.length - 1) {
+                    List<Integer> historyList = historyFromString(lines[lines.length - 1]);
                     for (int id : historyList) {
                         fromFile.getHistoryManager().add(fromFile.getAnyTask(id));
                     }
                 }
             }
             return fromFile;
-        } catch (IOException e){
-            throw new ManagerLoadException("Не загрузить  данные " + "\n ошибка",e.getCause());
+        } catch (IOException e) {
+            throw new ManagerLoadException("Не загрузить  данные " + "\n ошибка", e.getCause());
         }
     }
 
@@ -122,7 +122,7 @@ public class FileBackedTasksManager extends InMemoryTaskManager {
 
 
         } catch (IOException e) {
-            throw new ManagerSaveException("Не удалось сохранит данные " + "\n ошибка",e.getCause());
+            throw new ManagerSaveException("Не удалось сохранит данные " + "\n ошибка", e.getCause());
         }
     }
 
@@ -140,64 +140,64 @@ public class FileBackedTasksManager extends InMemoryTaskManager {
 
     public Task fromString(String str) {
 
-            String[] element = str.split(",");// разбили строку по запятой
-            int id = Integer.parseInt(element[0]);//создали поле айди через парсинт
-            TaskType taskType; //создали поле тасктайп
-            if (element[1].equals(TaskType.TASK.toString())) { // сравнили значение в массиве со значением из энама через тустринг
-                taskType = TaskType.TASK; // присвоили тасктайпу значение по энаму
-            } else if (element[1].equals(TaskType.SUBTASK.toString())) {
-                taskType = TaskType.SUBTASK;
-            } else if (element[1].equals(TaskType.EPICTASK.toString())) {
-                taskType = TaskType.EPICTASK;
-            } else {
-                taskType = null;
+        String[] element = str.split(",");// разбили строку по запятой
+        int id = Integer.parseInt(element[0]);//создали поле айди через парсинт
+        TaskType taskType; //создали поле тасктайп
+        if (element[1].equals(TaskType.TASK.toString())) { // сравнили значение в массиве со значением из энама через тустринг
+            taskType = TaskType.TASK; // присвоили тасктайпу значение по энаму
+        } else if (element[1].equals(TaskType.SUBTASK.toString())) {
+            taskType = TaskType.SUBTASK;
+        } else if (element[1].equals(TaskType.EPICTASK.toString())) {
+            taskType = TaskType.EPICTASK;
+        } else {
+            taskType = null;
+        }
+        String name = element[2];
+        TaskStatus taskStatus = null; // создали поле таскстатус
+        if (element[3].equals(TaskStatus.NEW.toString())) { // сравнили значение в массиве со значением енама через тустринг
+            taskStatus = TaskStatus.NEW;  // присвоили таскстатусу значение по энаму
+        } else if (element[3].equals(TaskStatus.IN_PROGRESS.toString())) {
+            taskStatus = TaskStatus.IN_PROGRESS;
+        } else if (element[3].equals(TaskStatus.DONE.toString())) {
+            taskStatus = TaskStatus.DONE;
+        }
+        String desc = element[4];
+        Task task = null;
+
+        if (element.length == 6) { // если количество элементов = 6, то это сабтаск
+            int idEpic = Integer.parseInt(element[5]); // создали доп поле эпикайди через парсинт
+            task = new SubTask(name, desc, taskType, idEpic); // создали новую сабтаску
+            task.setStatus(taskStatus); // присвоили статус новой сабтаске
+            task.setId(id); // установили айди сабтаске
+        } else {
+
+            if (taskType == TaskType.TASK) { // если поле таски таска, то это таска
+                task = new Task(name, id, desc, taskStatus, taskType); // создали новую таску
+
+            } else { // иначе это эпиктаск
+                task = new EpicTask(name, desc, taskType);// создали новый эпик таск
+                task.setId(id);// присвоили айди
             }
-            String name = element[2];
-            TaskStatus taskStatus = null; // создали поле таскстатус
-            if (element[3].equals(TaskStatus.NEW.toString())) { // сравнили значение в массиве со значением енама через тустринг
-                taskStatus = TaskStatus.NEW;  // присвоили таскстатусу значение по энаму
-            } else if (element[3].equals(TaskStatus.IN_PROGRESS.toString())) {
-                taskStatus = TaskStatus.IN_PROGRESS;
-            } else if (element[3].equals(TaskStatus.DONE.toString())) {
-                taskStatus = TaskStatus.DONE;
-            }
-            String desc = element[4];
-            Task task = null;
-
-            if (element.length == 6) { // если количество элементов = 6, то это сабтаск
-                int idEpic = Integer.parseInt(element[5]); // создали доп поле эпикайди через парсинт
-                task = new SubTask(name, desc, taskType, idEpic); // создали новую сабтаску
-                task.setStatus(taskStatus); // присвоили статус новой сабтаске
-                task.setId(id); // установили айди сабтаске
-            } else  {
-
-                if (taskType == TaskType.TASK) { // если поле таски таска, то это таска
-                    task = new Task(name, id, desc, taskStatus, taskType); // создали новую таску
-
-                } else { // иначе это эпиктаск
-                    task = new EpicTask(name, desc, taskType);// создали новый эпик таск
-                    task.setId(id);// присвоили айди
-                }
-            }
-
-            return task;
         }
 
-     private static String historyToString(HistoryManager manager){
-        StringBuilder stringBuilder = new StringBuilder();// создали стрингбилдер
-        List<Task> tasks = manager.getHistory();// создали лист тасок из истории
-         for (Task task : tasks) {
-             stringBuilder.append(task.getId());
-             stringBuilder.append(",");
-         }//достали айди из таски сложили их через ","
-         return stringBuilder.toString(); //вернули стринговое значение стрингбилдера
+        return task;
     }
 
-    public static List<Integer> historyFromString(String str){
+    private static String historyToString(HistoryManager manager) {
+        StringBuilder stringBuilder = new StringBuilder();// создали стрингбилдер
+        List<Task> tasks = manager.getHistory();// создали лист тасок из истории
+        for (Task task : tasks) {
+            stringBuilder.append(task.getId());
+            stringBuilder.append(",");
+        }//достали айди из таски сложили их через ","
+        return stringBuilder.toString(); //вернули стринговое значение стрингбилдера
+    }
+
+    public static List<Integer> historyFromString(String str) {
         String[] element = str.split(",");
         List<Integer> list = new ArrayList<>();
-        for (int i = 0; i<element.length; i++) {
-            list.add(Integer.parseInt(element[i])) ;// добавили элементы массива в лист
+        for (int i = 0; i < element.length; i++) {
+            list.add(Integer.parseInt(element[i]));// добавили элементы массива в лист
         }
         return list;
     }
